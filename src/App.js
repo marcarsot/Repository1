61be0e7730ca37a10c85b635a95a9df520b80672
marcarsot <marcaysot@hotmail.com>
2022-06-06 14:51:03 -0400

    Cambio App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Hola Mundo mi nombre es Marcelo Araya Soto.
         </p>
         <a
           className="App-link"
